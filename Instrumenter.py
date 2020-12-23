8805c6211074d0c3f597c97a1fc3c867714bde26
Shaamyl Anwar <mshaamylanwar@gmail.com>
2020-12-23 17:44:08 +0500

    Added portocol to Instrumenter

--- Instrumenter.py
+++ Instrumenter.py
@@ -120,6 +120,25 @@ class Instrumenter:
           block = [smali.BLANK_LINE(), smali.COMMENT("IFT INSTRUCTIONS ADDED BY STIGMA " + comment_detail)]
           return block
     
+    @staticmethod
+    def create_logd_block(m, tag, message):
+        try:
+            tmp_reg_for_tag = m.make_new_reg()
+            tmp_reg_for_msg = m.make_new_reg()
+        except RuntimeError:
+            return 0
+
+        block = [smali.CONST_STRING(tmp_reg_for_tag, tag),
+                smali.BLANK_LINE(),
+                smali.CONST_STRING(tmp_reg_for_msg, message),
+                smali.BLANK_LINE(),
+                smali.LOG_D(tmp_reg_for_tag, tmp_reg_for_msg),
+                smali.BLANK_LINE()]
+
+        m.free_reg()
+        m.free_reg()
+        
+        return block
 
     @staticmethod
     def SIMPLE_instrumentation(scd, m, line_num, regex, dest_num, source_num, comment_string):
@@ -137,7 +156,7 @@ class Instrumenter:
         taint_field_dest = scd.create_taint_field(m.get_name(), regs[dest_num])
 
         block = Instrumenter.make_comment_block(comment_string)
-        block = block + make_simple_assign_block(scd, m, taint_field_dest, taint_field_src)
+        block = block + Instrumenter.make_simple_assign_block(scd, m, taint_field_dest, taint_field_src)
         block = block + Instrumenter.make_comment_block(comment_string)
         
         m.embed_block(line_num, block)
@@ -169,7 +188,7 @@ class Instrumenter:
         taint_loc_result = scd.create_taint_field(m.get_name(), result_reg)
 
         block = Instrumenter.make_comment_block("for FILLED-NEW-ARRAY")
-        block = block + make_merge_register_block(scd, m, regs, taint_loc_result)
+        block = block + Instrumenter.make_merge_register_block(scd, m, regs, taint_loc_result)
         block = block + Instrumenter.make_comment_block("for FILLED-NEW-ARRAY")
 
         m.embed_block(line_num, block)
@@ -365,6 +384,8 @@ class Instrumenter:
         except RuntimeError:
             return 0
 
+        logBlock = Instrumenter.create_logd_block(m, "\"STIGMAAA\"", "\"IMEI Num Entered\"")
+
         block = [smali.BLANK_LINE(),
                  smali.COMMENT("IFT INSTRUCTIONS ADDED BY STIGMA for getDeviceID()"),
                  smali.BLANK_LINE(),
@@ -373,6 +394,8 @@ class Instrumenter:
                  smali.SPUT(tmp_reg_for_constant, scd.class_name, taint_location),
                  smali.BLANK_LINE(),
                  smali.COMMENT("IFT INSTRUCTIONS ADDED BY STIGMA for getDeviceID()")]
+
+        block = logBlock + block
 
         m.embed_block(line_num, block)
         lines_embedded = len(block)
@@ -398,20 +421,22 @@ class Instrumenter:
         except RuntimeError:
             return 0
             
+        
+        logBlock = Instrumenter.create_logd_block(m, "\"STIGMAAB\"", "\"Phone Num Entered\"")
+        
         block = Instrumenter.make_comment_block("for getLine1Number()")
         
+     
         block.append(smali.BLANK_LINE())
         block.append(smali.CONST_16(tmp_reg_for_constant, "0x1"))
         block.append(smali.BLANK_LINE())
         block.append(smali.SPUT(tmp_reg_for_constant, scd.class_name, taint_loc))
         
-        block = block + Instrumenter.make_comment_block("for getLine1Number()")
+        block = logBlock + block + Instrumenter.make_comment_block("for getLine1Number()")
         
         m.embed_block(line_num, block)
         m.free_reg() # 1
         return len(block)
-        
-            
 
     @staticmethod
     def WRITE_instrumentation(scd, m, line_num):  # "write()" sinks
@@ -451,7 +476,7 @@ class Instrumenter:
                  smali.BLANK_LINE(),
                  smali.IF_EQZ(tmp_reg_for_compare, jmp_label),
                  smali.BLANK_LINE(),
-                 smali.CONST_STRING(tmp_reg_for_tag, "\"STIGMA\""),
+                 smali.CONST_STRING(tmp_reg_for_tag, "\"STIGMAZZ\""),
                  smali.BLANK_LINE(),
                  smali.CONST_STRING(tmp_reg_for_msg, "\"LEAK via WRITE() OCCURING!\""),
                  smali.BLANK_LINE(),
@@ -509,7 +534,7 @@ class Instrumenter:
                  smali.BLANK_LINE(),
                  smali.IF_EQZ(tmp_reg_for_compare, jmp_label),
                  smali.BLANK_LINE(),
-                 smali.CONST_STRING(tmp_reg_for_tag, "\"STIGMA\""),
+                 smali.CONST_STRING(tmp_reg_for_tag, "\"STIGMAZY\""),
                  smali.BLANK_LINE(),
                  smali.CONST_STRING(tmp_reg_for_msg, "\"LEAK via LOGD() OCCURING!\""),
                  smali.BLANK_LINE(),
