7f4ba1461158d68ae8dca17ab0d25deb8e4e0a07
Ed <ejnovak@email.wm.edu>
2020-12-30 17:45:57 -0500

    Config file for gplaycli to specify the (correct) locale

--- stigma/gplaycli/get_them_all.sh
+++ stigma/gplaycli/get_them_all.sh
@@ -2,4 +2,7 @@
 
 # https://github.com/matlink/gplaycli
 
-xargs gplaycli --verbose --progress --download < package_ids.txt
+# my original log file was here "/usr/local/etc/gplaycli/gplaycli.conf"
+
+
+xargs gplaycli -c ./gplaycli.conf --verbose --progress --download < package_ids.txt
