a531c5f3841c116fb54fda2387ff112766fa65b9
ncarpent2026 <ncarpent@fandm.edu>
2025-05-23 12:05:32 -0400

    Sill working on Slicing.py

--- Slicing.py
+++ Slicing.py
@@ -6,14 +6,7 @@ import SmaliAssemblyInstructions
 import StigmaStringParsingLib
 import SmaliCodeIterator
 
-def get_function_name(filename, line_number):
-    fh = open(filename, "r")
-    lines = fh.readlines()
-    fh.close()
-
-    # account for index 0
-    line_number -= 1
-
+def get_function_name(filename, line_number, lines):
     match_object = re.match(StigmaStringParsingLib.BEGINS_WITH_DOT_METHOD, lines[line_number])
     while(match_object == None):
         line_number -= 1
@@ -32,17 +25,48 @@ def find_smali_method_def_obj(method_signature_str, smali_class):
 
     return curr_method
 
+def test_instance(instruction, reg):
+    # MOVE instances: if reg is origin, add destination to regs
+    # Destination is first
+    # If reg is destination, remove from regs
+    if isinstance(instruction, SmaliAssemblyInstructions.MOVE):
+        print("MOVE")
+        if not isinstance(instruction, SmaliAssemblyInstructions._SINGLE_REGISTER_INSTRUCTION):
+            instruction_regs = instruction.get_registers()
+            if instruction_regs[1] == reg:
+                print("TARGET IS ORIGIN, ADD DESTINATION " + str(instruction_regs[0]))
+                return instruction_regs[0]
+            else: # instruction_regs[0] == reg
+                print("TARGET IS DESTINATION, OVERWRITTEN")
+                return "REMOVE CURRENT"
+    elif isinstance(instruction, SmaliAssemblyInstructions.IGET_OBJECT):
+        print("IGET")
+        return "v4"
+    elif isinstance(instruction, SmaliAssemblyInstructions.IPUT_OBJECT):
+        print("IPUT")
+        return "v4"
+    else:
+        return "null"
+
 
 def main(filename, line_number, reg):
+    fh = open(filename, "r")
+    lines = fh.readlines()
+    fh.close()
+
+    # account for index 0
+    line_number -= 1
+
     smali_reg = SmaliRegister.SmaliRegister(reg)
     smali_class = SmaliClassDef.SmaliClassDef(filename)
 
-    method_signature_str = get_function_name(filename, line_number)
+    method_signature_str = get_function_name(filename, line_number, lines)
     smali_method_def_obj = find_smali_method_def_obj(method_signature_str, smali_class)
     full_text = SmaliCodeIterator.SmaliCodeIterator(smali_method_def_obj.raw_text)
 
     # forward tracing
-    signature_string_found = False
+    target_line = lines[line_number]
+    target_line_found = False
 
     registers_to_check = []
     registers_to_check.append(reg)
@@ -50,33 +74,23 @@ def main(filename, line_number, reg):
     for line in SmaliCodeIterator.SmaliCodeIterator(smali_method_def_obj.raw_text):
         line = "".join(line)
 
-        for register in registers_to_check:
-            if line.replace("\n", "") == method_signature_str:
-                signature_string_found = True
-            if register in line and signature_string_found:
-                instruction = SmaliAssemblyInstructions.SmaliAssemblyInstruction().from_line(line)
+        if line.replace("\n", "") == target_line.replace("\n", ""):
+            print("Target found")
+            target_line_found = True
 
-                # MOVE instances: if reg is origin, add destination to regs
-                # Destination is first
-                # If reg is destination, remove from regs
-                if isinstance(instruction, SmaliAssemblyInstructions.MOVE):
-                    print("MOVE")
+        if target_line_found:
+            for register in registers_to_check:
+                if register in line:
+                    print("register " + register + " is in line " + line)
+                    instruction = SmaliAssemblyInstructions.SmaliAssemblyInstruction().from_line(line)
+                    reg_to_add = test_instance(instruction, register)
 
-                    # only dealing with two register instructions right now, will deal with the others later
-                    if not isinstance(instruction, SmaliAssemblyInstructions._SINGLE_REGISTER_INSTRUCTION):
-                        instruction_regs = instruction.get_registers()
-                        if instruction_regs[0] == reg:
-                            print("TARGET IS DESTINATION, OVERWRITTEN")
-                            #registers_to_check.remove(reg)
-                        else: # instruction_regs[1] == reg
-                            print("TARGET IS ORIGIN, ADD DESTINATION " + str(instruction_regs[0]))
-                            #registers_to_check.append(instruction_regs[0])
+                    if reg_to_add == "REMOVE CURRENT":
+                        registers_to_check.remove(register)
+                    elif reg_to_add not in registers_to_check:
+                        registers_to_check.append(reg_to_add)
 
-                # GET instances
-
-                print(instruction)
-
-        #signature_string_found = False
+                    break # only log the line once
             
                 
 main("SendMessagesHelper.smali", 27946, "v4")
