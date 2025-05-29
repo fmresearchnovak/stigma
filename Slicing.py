eb5f6dd31cca9a1faef3571ae2faa6ef08b93d01
ncarpent2026 <ncarpent@fandm.edu>
2025-05-29 16:27:49 -0400

    new forward_tracing method replacing old one, recursive process commented out

--- Slicing.py
+++ Slicing.py
@@ -1,12 +1,15 @@
 import re
 import argparse
 import subprocess
+import time
+import os
 
 import SmaliRegister
 import SmaliClassDef
 import SmaliAssemblyInstructions
 import StigmaStringParsingLib
 import SmaliCodeIterator
+import StigmaState
 
 def findAPK(apk):
     if (not os.path.exists(apk)):
@@ -32,6 +35,13 @@ def find_smali_method_def_obj(method_signature_str, smali_class):
         curr_method = smali_class.methods[method_index]
 
     return curr_method
+
+def grep_test(target, path):
+    cmd = ["grep", target, "-r", path]
+    grep_result = subprocess.run(cmd, stdout = subprocess.PIPE)
+    uses_list = str(grep_result.stdout)[2:].split("\\n")
+    for use in uses_list:
+        print(use)
 
 def test_instance(instruction, location, original_line):
     first = False
@@ -63,11 +73,11 @@ def test_instance(instruction, location, original_line):
     # if target is destination, overwrite
     # if target is object, do nothing
     # if target is instance variable, add destination
-    elif isinstance(instruction, SmaliAssemblyInstructions.IGET_OBJECT):
+    elif isinstance(instruction, SmaliAssemblyInstructions.IGET_OBJECT) or isinstance(instruction, SmaliAssemblyInstructions.IGET):
         print("IGET")
         instance_var = instruction.get_instance_variable()
         if instance_var == location:
-            print("ADDING NEW LOCATION " + instruction_regs[0])
+            print("ADDING NEW LOCATION " + str(instruction_regs[0]))
             return instruction_regs[0]
         else: # instruction_regs[0] = location
             if first:
@@ -82,7 +92,7 @@ def test_instance(instruction, location, original_line):
     # if target is origin, add instance variable
     # if target is object, do nothing
     # if target is instance variable, overwrite
-    elif isinstance(instruction, SmaliAssemblyInstructions.IPUT_OBJECT):
+    elif isinstance(instruction, SmaliAssemblyInstructions.IPUT_OBJECT) or isinstance(instruction, SmaliAssemblyInstructions.IPUT):
         print("IPUT")
         instance_var = instruction.get_instance_variable()
         if instruction_regs[0] == location:
@@ -103,7 +113,10 @@ def test_instance(instruction, location, original_line):
 
 
 # REWRITTEN VERSION OF FUNCTION BELOW
-def forward_tracing(filename, line_number, location, files_to_search, line_directory):
+def forward_tracing(filename, line_number, location, files_to_search, line_directory, tmp_file_name):
+    print("=================================")
+    print(filename)
+    print("=================================")
 
     # STEP 1: Create list of registers (and instance variables) to check for
     locations_to_check = []
@@ -139,7 +152,7 @@ def forward_tracing(filename, line_number, location, files_to_search, line_direc
         # STEP 6: Once the target line is found, send every line containing a location in locations_to_track to the test_instance function
         if target_line_found:
             for location in locations_to_check:
-                if location in line:
+                if (location + ",") in line or (location in line and ";" in location):
                     print("----------------------------------------------------")
                     print("LOCATION = " + location)
                     print("LINE = " + line)
@@ -147,16 +160,21 @@ def forward_tracing(filename, line_number, location, files_to_search, line_direc
 
                     instruction = SmaliAssemblyInstructions.SmaliAssemblyInstruction().from_line(line)
                     loc_to_add = str(test_instance(instruction, location, target_line))
+                    print("loc to add = " + str(loc_to_add))
 
                     if loc_to_add == "REMOVE CURRENT":
                         locations_to_check.remove(location)
                     elif loc_to_add not in locations_to_check:
                         locations_to_check.append(loc_to_add)
+                        print("Added to locations")
+
+                    print(locations_to_check)
 
                     # STEP 7: For every instance variable added to tracking that is not in the current file, add the file name and lines to line_directory
                     if isinstance(instruction, SmaliAssemblyInstructions._I_INSTRUCTION):
+                        print(str(instruction) + " is I_INSTRUCTION, find instance variables throughout files...")
                         # something to specify folder
-                        cmd = ["grep", str(instruction.get_instance_variable()).replace("\n", ""), "-r"]
+                        cmd = ["grep", str(instruction.get_instance_variable()).replace("\n", ""), "-r", tmp_file_name]
                         grep_result = subprocess.run(cmd, stdout = subprocess.PIPE)
 
                         # result is a list of uses of the instance variable
@@ -164,6 +182,7 @@ def forward_tracing(filename, line_number, location, files_to_search, line_direc
                         uses_list.pop()
 
                         for use in uses_list:
+                            #print(use)
                             use = use.split(":", 1)
 
                             file = use[0]
@@ -184,25 +203,42 @@ def forward_tracing(filename, line_number, location, files_to_search, line_direc
     if len(files_to_search) > 0:
         new_file = files_to_search[0]
         new_instance_to_check = line_directory[new_file][0]
+        
         line_directory[new_file].pop(0)
 
         if len(line_directory[new_file]) == 0:
             files_to_search.remove(new_file)
 
-        fh = open(filename, "r")
-        lines = fh.readlines()
+        fh = open(new_file, "r")
+        #lines = fh.readlines()
+
+
+        # get the location name not the line name
+        new_location = new_instance_to_check.split(" ", 3)[3]
+        new_location = new_location[:len(new_location) - 1]
+
+        # loop through to get line
+        new_line_number = 0
+        target_line = new_location
+        curr_line = ""
+        while target_line not in curr_line:
+            new_line_number += 1
+            curr_line = fh.readline()
+
         fh.close()
 
-        new_line_number = lines.index(new_instance_to_check) + 1
+        #new_line_number = lines.index(new_instance_to_check) + 1
 
         # STEP 9: Repeat all of the steps for the new file and its locations to check
-        return forward_tracing(new_file, new_line_number, new_instance_to_check, files_to_search, line_directory)
+        print("NEW ITERATION WITH " + new_file + " " + str(new_line_number) + " " + new_location)
+        #return forward_tracing(new_file, new_line_number, new_location, files_to_search, line_directory, tmp_file_name)S
+        return
 
     else:
         # return activity_log
+        return
 
         
-
 def trace(filename, line_number, reg):
     fh = open(filename, "r")
     lines = fh.readlines()
@@ -287,21 +323,38 @@ def trace(filename, line_number, reg):
 def main():
     parser = argparse.ArgumentParser(description = "Given a line of code and a register to track, traces the contents of the register throughout the process.")
 
-    #parser.add_argument("APK", help="The path to the APK file that the target file is located in.")
+    parser.add_argument("APK", help="The path to the APK file that the target file is located in.")
     parser.add_argument("filename", help="The file that contains the target line of code.")
     parser.add_argument("line_number", help="Line number of the line of code containing a reference to the desired register.")
     parser.add_argument("register", help="The register that you wish to trace the data of.")
 
+    args = parser.parse_args()
+    apk_path = findAPK(args.APK)
+
     # https://stackoverflow.com/questions/69981912/why-i-am-getting-this-error-typeerror-namespace-object-is-not-subscriptable
     args = parser.parse_args()
 
-    #args.APK = findAPK(args.APK)
+    temp_APK = StigmaState.Environment().get_temp_file()
+    tmp_file_name = StigmaState.Environment().get_temp_file().name
+    start_time = time.time()
+    cmd = ["java", "-jar", "pre-builts/apktool.jar", "d", apk_path, "-o", tmp_file_name, "-f"]
+    if (os.name == "nt"):
+        completed_process = subprocess.run(cmd, shell=True)
+    elif (os.name == "posix"):
+        completed_process = subprocess.run(cmd)
+    completed_process.check_returncode()
+    print("Apk unpacked in %.1f seconds" % (time.time() - start_time))
 
-    #trace(os.path.join(args.APK, args.filename), int(args.line_number), args.register)
-    trace(args.filename, int(args.line_number), args.register)
+    #trace(args.filename, int(args.line_number), args.register)
+    forward_tracing(args.filename, int(args.line_number), args.register, [], {}, tmp_file_name)
 
-    #APK stuff doesn't work yet
-
+    #grep_test("Lorg/telegram/tgnet/TLRPC$Message;->media:Lorg/telegram/tgnet/TLRPC$MessageMedia", tmp_file_name)
 
 main()
+
 #main("SendMessagesHelper.smali", 27946, "v4")
+
+# LOOK AT
+# NEW ITERATION WITH /tmp/apkOutput_45yq44ko/smali_classes4/org/telegram/messenger/SecretChatHelper.smali 6501 Lorg/telegram/tgnet/TLRPC$MessageMedia;->last_name:Ljava/lang/String
+# NEW ITERATION WITH /tmp/apkOutput_45yq44ko/smali_classes4/org/telegram/messenger/SecretChatHelper.smali 6505 Lorg/telegram/tgnet/TLRPC$MessageMedia;->first_name:Ljava/lang/String
+# MAKE STATEMENT TO PREVENT GOING TO THE SAME LINE OF A FILE TWICE
